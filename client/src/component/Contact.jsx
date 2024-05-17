73bd7de750e9c73618c59fae98bffc6761b28a72
kamranmughals <kami302716@gmail.com>
2024-05-17 05:14:37 +0500

    5/17/2024 5:07 AM

--- client/src/component/Contact.jsx
+++ client/src/component/Contact.jsx
@@ -1,121 +1,126 @@
 import React, { useEffect } from "react";
 import Footer from "./Footer";
-import bg  from "../images/Contact us.gif"
-import "../style/contact.css"
-import AOS from 'aos';
-import 'aos/dist/aos'
+import bg from "../images/Contact us.gif";
+import "../style/contact.css";
+import AOS from "aos";
+import "aos/dist/aos";
 import Navbar from "./Navbar";
 
-
 function Contact() {
-
-    useEffect(()=>{
-        AOS.init({duration: 2000})
-    })
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  });
   return (
     <>
-        <Navbar />
+      <Navbar />
 
-    <div className="container custom-container mb-4">
-    <div className="container p-5" data-aos="fade-up">
-        <div className="text-center">
-          <label className="contact-tag">Contact US</label>
+      <div className="container custom-container mb-4">
+        <div className="container p-5" data-aos="fade-up">
+          <div className="text-center">
+            <label className="contact-tag">Contact US</label>
+          </div>
         </div>
-      </div>
-      <div className="container  pr-5" data-aos="fade-up">
-        <div className="row justify-content-center">
-          <div className="col-md-11">
-            <div className="row">
-              <div className="col-md-3 col-lg-4">
-                <h3 className="heading mb-4 font-weight-bolder">Let's talk about everything!</h3>
-                <p>
-                  Please send your feedback about our services, in regards
-                </p>
-                <p>
-                  <img src={bg} alt={bg} className="img-fluid" />
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-4">
-                <form
-                  className="mb-5"
-                  method="post"
-                  id="contactForm"
-                  name="contactForm"
-                >
-                  <div className="row">
-                    <div className="col-md-12 form-group">
-                      <input
-                        type="text"
-                        className="form-control form-control-lg"
-                        name="name"
-                        id="name"
-                        placeholder="Your name"
-                      />
+        <div className="container  pr-5" data-aos="fade-up">
+          <div className="row justify-content-center">
+            <div className="col-md-11">
+              <div className="row">
+                <div className="col-md-3 col-lg-4">
+                  <h3 className="heading mb-4 font-weight-bolder">
+                    Let's talk about everything!
+                  </h3>
+                  <p>
+                    Please send your feedback about our services, in regards
+                  </p>
+                  <p>
+                    <img src={bg} alt={bg} className="img-fluid" />
+                  </p>
+                </div>
+                <div className="col-md-3 col-lg-4">
+                  <form
+                    className="mb-5"
+                    method="post"
+                    id="contactForm"
+                    name="contactForm"
+                  >
+                    <div className="row">
+                      <div className="col-md-12 form-group">
+                        <input
+                          type="text"
+                          className="form-control form-control-lg"
+                          name="name"
+                          id="name"
+                          placeholder="Your name"
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-12 frm form-group ">
-                      <input
-                        type="text"
-                        className="form-control form-control-lg"
-                        name="email"
-                        id="email"
-                        placeholder="Email"
-                      />
+                    <div className="row">
+                      <div className="col-md-12 frm form-group ">
+                        <input
+                          type="text"
+                          className="form-control form-control-lg"
+                          name="email"
+                          id="email"
+                          placeholder="Email"
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-12 form-group">
-                      <input
-                        type="text"
-                        className="form-control  form-control-lg"
-                        name="subject"
-                        id="subject"
-                        placeholder="Subject"
-                      />
+                    <div className="row">
+                      <div className="col-md-12 form-group">
+                        <input
+                          type="text"
+                          className="form-control  form-control-lg"
+                          name="subject"
+                          id="subject"
+                          placeholder="Subject"
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-12 form-group">
-                      <textarea
-                        className="form-control  form-control-lg"
-                        name="message"
-                        id="message"
-                        cols="30"
-                        rows="7"
-                        placeholder="Write your message"
-                      ></textarea>
+                    <div className="row">
+                      <div className="col-md-12 form-group">
+                        <textarea
+                          className="form-control  form-control-lg"
+                          name="message"
+                          id="message"
+                          cols="30"
+                          rows="7"
+                          placeholder="Write your message"
+                        ></textarea>
+                      </div>
                     </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-12">
-                      <input
-                        type="submit"
-                        value="Send Message"
-                        className="btn btn-primary btn-custom shadow py-2 px-4"
-                      />
-                      <span className="submitting"></span>
+                    <div className="row">
+                      <div className="col-12">
+                        <input
+                          type="submit"
+                          value="Send Message"
+                          className="btn btn-primary btn-custom shadow py-2 px-4"
+                        />
+                        <span className="submitting"></span>
+                      </div>
                     </div>
-                  </div>
-                </form>
-                <div id="form-message-warning mt-4"></div>
-                <div id="form-message-success"></div>
-              </div>
-              <div class="col-md-3 col-lg-3">
+                  </form>
+                  <div id="form-message-warning mt-4"></div>
+                  <div id="form-message-success"></div>
+                </div>
+                <div class="col-md-3 col-lg-3">
                   <div class="map_main">
-                     <div class="map-responsive">
-
-                        <iframe className="frame" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&amp;q=Eiffel+Tower+Paris+France" width="350" height="490" frameborder="0"  allowfullscreen="" title="map"></iframe>
-                     </div>
+                    <div class="map-responsive">
+                      <iframe
+                        className="frame"
+                        src="https://www.google.com/maps/embed/v1/place?key=ADDYOURKEY&amp;q=Eiffel+Tower+Paris+France"
+                        width="350"
+                        height="490"
+                        frameborder="0"
+                        allowfullscreen=""
+                        title="map"
+                      ></iframe>
+                    </div>
                   </div>
-               </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-    </div>
 
-
-    
         {/* <h2 className="h1-responsive font-weight-bold text-center my-4">
           Contact us
         </h2>
